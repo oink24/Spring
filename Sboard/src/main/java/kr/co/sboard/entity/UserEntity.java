f6811097b4c466d5dbfa52d5a0f700728f7955e2
oink24 <oooq3o@gmail.com>
2023-10-11 16:42:18 +0900

    Spring 기반 게시판 프로젝트

---
+++ Sboard/src/main/java/kr/co/sboard/entity/UserEntity.java
@@ -0,0 +1,37 @@
+package kr.co.sboard.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "User")
+public class UserEntity {
+
+    @Id
+    private String uid;
+    private String pass;
+    private String name;
+    @Column(name = "nickname", unique = true)
+    private String nickname;
+    @Column(name = "email", unique = true)
+    private String email;
+    @Column(name = "hp", unique = true)
+    private String hp;
+    private String role;
+    private String zip;
+    private String addr1;
+    private String addr2;
+    private String regip;
+    private LocalDateTime regdate;
+    private LocalDateTime leaveDate;
+}
