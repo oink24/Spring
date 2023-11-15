7cec6e6c3cfb5835a25d2398f01d3f3cfa8a2f75
oink24 <oooq3o@gmail.com>
2023-11-15 19:57:49 +0900

    Swagger 실습

---
+++ Ch10/src/main/java/kr/ch10/config/SwaggerConfiguration.java
@@ -0,0 +1,25 @@
+package kr.ch10.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfiguration {
+
+    // http://localhost:8080/Ch10/v3/api-docs
+    // http://localhost:8080/Ch10/swagger-ui/index.html
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("Swagger API 제목")
+                .version("1.0")
+                .description("API에 대한 설명 부분");
+
+        return new OpenAPI()
+                .components(new Components())
+                .info(info);
+    }
+}
